d705eee28ea6691e7b0778b220f8564422d1a449
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-07-24 02:52:06 +0530

    NavBar - Modified design - Responsive

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { TiThMenu } from "react-icons/ti";
+import "./navbar.css";
+// , "About Us", "Services"
+export default function NavBar() {
+    const [menu, setMenu] = useState(false);
+    const menuItems = ["Home", "Projects", "Services", "About Us", "Contact"];
+
+    return (
+        <header className="navbar">
+            <div className="navbar__menu--icon" onClick={() => setMenu(!menu)}>
+                <TiThMenu />
+            </div>
+            <div
+                className={
+                    menu
+                        ? "navbar__menu--mobile navbar__menu--mobile--show navbar__menu--desktop"
+                        : "navbar__menu--mobile navbar__menu--mobile--hide navbar__menu--desktop"
+                }
+            >
+                <ul>
+                    {menuItems.map((item) => (
+                        <li
+                            className="navbar__menu__item navbar__menu__item--hover"
+                            onClick={() => setMenu(false)}
+                        >
+                            <a href="./">{item}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </header>
+    );
+}
