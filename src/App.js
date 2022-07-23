0133d328f1112b14416525dce8adbd03fdba5ca1
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-07-23 22:44:15 +0530

    Clean folder - Deleted unnecessary files and some styling changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import "./App.css";
-import Nav from './components/Nav'
+import Nav from "./components/Nav";
 import Intro from "./components/Intro";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
 function App() {
-    return <div className="App">
-        <Nav></Nav>
-        <Intro></Intro>
-        <Skills></Skills>
-        <Projects></Projects>
-        <Footer></Footer>
-    </div>;
+    return (
+        <div className="App">
+            <Nav></Nav>
+            <Intro></Intro>
+            <Skills></Skills>
+            <Projects></Projects>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
