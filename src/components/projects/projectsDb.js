1084106f6c89bf7860ee11541aad4481446357d0
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-07-26 01:55:09 +0530

    Projects section designed as a component

---
+++ src/components/projects/projectsDb.js
@@ -0,0 +1,151 @@
+const projectsDB = [
+    {
+        title: "Todo list App",
+        description:
+            "An application to create, read, update, and delete To-Do items",
+        live_url: "https://nivtodo.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/todo",
+    },
+    {
+        title: "Weather Cast App",
+        description:
+            "Web app which can display weather cast for the users locality and provided with a search option as well",
+        live_url: "https://weatherrcast.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/weather-app",
+    },
+    {
+        title: "Tints and Shades generator",
+        description: "Generates various tints and shades for a given color",
+        live_url: "https://tintsnshades.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/tints-shades",
+    },
+    {
+        title: "QnA section",
+        description:
+            "A component similar to QnA or FAQs section with Accordion design",
+        live_url: "https://nivqna.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/qna-section",
+    },
+    {
+        title: "Random text generator",
+        description: "Generates random text with the desired no of paragraphs",
+        live_url: "https://textgenaration.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/text-generator",
+    },
+    {
+        title: "Forms with React JS",
+        description: "A form implemented and handled using React JS",
+        live_url: "https://nivform.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/react-form",
+    },
+
+    {
+        title: "Filter powered Menu",
+        description:
+            "A responsive component designed to showcase menu items with filtering feature",
+        live_url: "https://nivmenu.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/react-menu",
+    },
+    {
+        title: "Tours App",
+        description: "A Tours app using React JS",
+        live_url: "https://traveldiariess.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/tours-app",
+    },
+    {
+        title: "Tic Tac Toe Game",
+        description: "A GUI based game designed using React JS",
+        live_url: "https://niv-tictactoe.netlify.app/",
+        code_url: "https://github.com/srinivasreddysagi/tic-tac-toe",
+    },
+    {
+        title: "Real-time currency converter",
+        description:
+            "A real-time currency converter developed using Vanilla JS and Exchange Rate API.",
+        live_url: "https://niv-js.netlify.app/currency-converter/",
+        code_url: "https://github.com/srinivasreddysagi/currency-converter",
+    },
+
+    {
+        title: "Expense Tracker",
+        description:
+            "An Expense Tracker built using HTML, CSS, JavaScript and Local Storage.",
+        live_url: "https://niv-js.netlify.app/expense-tracker/",
+        code_url: "https://github.com/srinivasreddysagi/expense-tracker/",
+    },
+
+    {
+        title: "Form validations",
+        description:
+            "Client side form validation using HTML, CSS, JavaScript and Regex.",
+        live_url: "https://niv-js.netlify.app/form-validations/",
+        code_url: "https://github.com/srinivasreddysagi/form-validations/",
+    },
+
+    {
+        title: "Rock Paper Scissors Game",
+        description:
+            "A simple UI based Rock Paper Scissors game built using HTML, CSS and JavaScript. Have fun!",
+        live_url: "https://niv-js.netlify.app/game-rock-paper-scissors/",
+        code_url:
+            "https://github.com/srinivasreddysagi/game-rock-paper-scissors/",
+    },
+
+    {
+        title: "Fylo landing page",
+        description:
+            "A beautiful and responsive webpage with pleasing look using grid layout",
+        live_url:
+            "https://niv-frontend.netlify.app/fylo-landing-page-with-two-column-layout-master/",
+        code_url:
+            "https://github.com/srinivasreddysagi/fylo-landing-page-with-two-column-layout-master/",
+    },
+
+    {
+        title: "Clipboard landing page",
+        description:
+            "A beautiful and responsive webpage using flex layout containing few individual sections",
+        live_url:
+            "https://niv-frontend.netlify.app/clipboard-landing-page-master/",
+        code_url:
+            "https://github.com/srinivasreddysagi/clipboard-landing-page-master/",
+    },
+
+    {
+        title: "Chat app component",
+        description:
+            "A splendid and complex chat app component designed using HTML and illustrated using CSS",
+        live_url:
+            "https://niv-frontend.netlify.app/chat-app-css-illustration-master/",
+        code_url:
+            "https://github.com/srinivasreddysagi/chat-app-css-illustration-master/",
+    },
+
+    {
+        title: "Testimonials grid section",
+        description: "An elegant testimonials section using flex layout",
+        live_url:
+            "https://niv-frontend.netlify.app/testimonials-grid-section-main/",
+        code_url:
+            "https://github.com/srinivasreddysagi/testimonials-grid-section-main/",
+    },
+
+    {
+        title: "Order summary component",
+        description:
+            "A simple and responsive order summary component using flex layout",
+        live_url:
+            "https://niv-frontend.netlify.app/order-summary-component-main/",
+        code_url:
+            "https://github.com/srinivasreddysagi/order-summary-component-main/",
+    },
+
+    // {
+    //     title: "",
+    //     description: "",
+    //     live_url: "",
+    //     code_url: "",
+    // }
+];
+
+export default projectsDB;
